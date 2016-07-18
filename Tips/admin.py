3143e34d970e207eb189f157e175300999b27ab2
jasmeet <jasmeet@dentalcareapp.com>
2016-07-17 21:36:08 -0400

    Revert "First Web commit"
    This reverts commit 2a85d8d3eafb9713f0b1e95e185a40b69f5b7dcc.

--- Tips/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Tip
-
-
-class TipsAdmin(admin.ModelAdmin):
-    fields = ['title','desc','image','owner']
-
-admin.site.register(Tip, TipsAdmin)
--- dentist_login/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DentistTestConfig(AppConfig):
-    name = 'dentist'
